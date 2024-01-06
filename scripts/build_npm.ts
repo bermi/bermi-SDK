5a93fe64256675a8d58fd1b892957627d432097b
Bermi Ferrer <bermi@bermilabs.com>
2024-01-06 19:22:32 +0100

    Pinning dependency to reproduce build

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -1,4 +1,4 @@
-import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+import { build, emptyDir } from "https://deno.land/x/dnt@0.33.1/mod.ts";
 import { VERSION } from "../version.ts";
 
 await emptyDir("./npm");
